0888b4773e53c8222efbf17185d034bbf3a5ff16
kiana-jahanshid <64160703+kiana-jahanshid@users.noreply.github.com>
2023-03-24 01:27:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -8,71 +8,98 @@ from PySide6.QtUiTools import QUiLoader
 from mytime import MyTime
 from timer import TimerThread
 from stopwatch import StopWatchThread
+from world_clock import WorldTimeThread
+from main_window import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self ):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self )
+
+
+        def reset_stopwatch():
+            self.thread_stopwatch.reset()
+            self.ui.label_stopwatch.setText("0:0:0")
+        def stop_stopwatch():
+            self.thread_stopwatch.terminate()  
+        def start_stopwatch():
+            self.thread_stopwatch.start() 
+        def show_time_stopwatch(time):  
+            self.ui.label_stopwatch.setText(f"{time.hour}:{time.minute}:{time.second}")
+
+
+        def start_timer():
+            self.thread_timer.start()
+        def show_time_timer(time):
+            self.textbox_hour_timer.setText(str(time.hour))
+            self.textbox_minute_timer.setText(str(time.minute))
+            self.textbox_second_timer.setText(str(time.second))
+            if time.hour == 0 and time.minute == 0 and time.second == 0 :
+                self.thread_timer.terminate()
+                alarm = QMessageBox()
+                alarm.setText(" ⚠⛔⏰ TIME IS OVER ⏰⛔⚠")
+                alarm.exec()
+
+        def get_text(time):
+            global text_hour , text_minute , text_second
+            text_hour   = self.textbox_hour_timer.text()
+            text_minute = self.textbox_minute_timer.text()                 
+            text_second = self.textbox_second_timer.text()
+            time.hour = int(text_hour)
+            time.minute = int(text_minute)
+            time.second = int(text_second)
+            show_time_stopwatch(time)
+            print(text_hour ,text_minute ,text_second   )
+                
+
+        
+        self.thread_stopwatch = StopWatchThread()
+        self.ui.label_stopwatch.setText("0:0:0")
+        self.ui.btn_start_stopwatch.clicked.connect(start_stopwatch)
+        self.ui.btn_stop_stopwatch.clicked.connect(stop_stopwatch)
+        self.ui.btn_reset_stopwatch.clicked.connect(reset_stopwatch)
+        self.thread_stopwatch.signal_send.connect(show_time_stopwatch)  # tooye thread , yek signale sakhtim , ke vaslesh mikonim be slote show_number
+
+
+        self.thread_timer = TimerThread()
+        self.ui.btn_start_timer.clicked.connect(start_timer)
+        self.thread_timer.signal_send.connect(show_time_timer)
+
+        self.ui.textbox_hour_timer.textChanged.connect(get_text)
+        self.ui.textbox_minute_timer.textChanged.connect(get_text)
+        self.ui.textbox_second_timer.textChanged.connect(get_text)
+        self.thread_timer.signal_send.connect(get_text)
 
 
 
-def reset_stopwatch():
-    thread_stopwatch.reset()
-    window.label_stopwatch.setText("0:0:0")
+        self.ir_time_thread = WorldTimeThread()
+        self.gr_time_thread = WorldTimeThread()
+        self.us_time_thread = WorldTimeThread()
 
-def stop_stopwatch():
-    thread_stopwatch.terminate()  
+        def start_Wclock():
+            self.ir_time_thread.start() 
+            self.gr_time_thread.start()
+            self.us_time_thread.start()
 
-def start_stopwatch():
-    thread_stopwatch.start() 
+        def show_time_Wclock2( time2 ):  
+            self.ui.gr_time.setText(f"{time2.hour}:{time2.minute}:{time2.second}")
 
-def show_time_stopwatch(time):  
-    window.label_stopwatch.setText(f"{time.hour}:{time.minute}:{time.second}")
+        def show_time_Wclock1( time1 ):  
+            self.ui.ir_time.setText(f"{time1.hour}:{time1.minute}:{time1.second}")
 
+        def show_time_Wclock3( time3 ):  
+            self.ui.us_time.setText(f"{time3.hour}:{time3.minute}:{time3.second}")
+                   
+        start_Wclock()
+        self.ir_time_thread.signal_send_i.connect(show_time_Wclock1)
+        self.gr_time_thread.signal_send_g.connect(show_time_Wclock2)
+        self.us_time_thread.signal_send_u.connect(show_time_Wclock3)
 
-def start_timer():
-    thread_timer.start()
-
-def show_time_timer(time):
-    window.textbox_hour_timer.setText(str(time.hour))
-    window.textbox_minute_timer.setText(str(time.minute))
-    window.textbox_second_timer.setText(str(time.second))
-    if time.hour == 0 and time.minute == 0 and time.second == 0 :
-        thread_timer.terminate()
-        alarm = QMessageBox()
-        alarm.setText(" ⚠⛔⏰ TIME IS OVER ⏰⛔⚠")
-        alarm.exec()
-
-def get_text(time):
-    global text_hour , text_minute , text_second
-    text_hour   = window.textbox_hour_timer.text()
-    text_minute = window.textbox_minute_timer.text()                 
-    text_second = window.textbox_second_timer.text()
-    time.hour = int(text_hour)
-    time.minute = int(text_minute)
-    time.second = int(text_second)
-    show_time_stopwatch(time)
-    print(text_hour ,text_minute ,text_second   )
-    
 
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
-    loader = QUiLoader()
-    window = loader.load("mainwindow.ui")
+    window = MainWindow()
     window.show()
-    
-
-    thread_stopwatch = StopWatchThread()
-    window.label_stopwatch.setText("0:0:0")
-    window.btn_start_stopwatch.clicked.connect(start_stopwatch)
-    window.btn_stop_stopwatch.clicked.connect(stop_stopwatch)
-    window.btn_reset_stopwatch.clicked.connect(reset_stopwatch)
-    thread_stopwatch.signal_send.connect(show_time_stopwatch)  # tooye thread , yek signale sakhtim , ke vaslesh mikonim be slote show_number
-
-
-    thread_timer = TimerThread()
-    window.btn_start_timer.clicked.connect(start_timer)
-    thread_timer.signal_send.connect(show_time_timer)
-
-    window.textbox_hour_timer.textChanged.connect(get_text)
-    window.textbox_minute_timer.textChanged.connect(get_text)
-    window.textbox_second_timer.textChanged.connect(get_text)
-    thread_timer.signal_send.connect(get_text)
-
     app.exec()
